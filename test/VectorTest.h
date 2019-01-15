c0a8fc86fc7f33088955226a00d6d588cbb299b0
hyf <37368470+fdHYF@users.noreply.github.com>
2019-01-15 19:32:02 +0800

    Create VectorTest.h

---
+++ test/VectorTest.h
@@ -0,0 +1,33 @@
+#ifndef VECTORTEST_H_INCLUDED
+#define VECTORTEST_H_INCLUDED
+
+#include "../vector.h"
+#include "TestHelp.h"
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+namespace TinySTL
+{
+    namespace VectorTest
+    {
+        void runAll();
+
+        void test1();
+        void test2();
+        void test3();
+        void test4();
+        void test5();
+        void test6();
+        void test7();
+        void test8();
+        void test9();
+        void test10();
+        void test11();
+        void test12();
+        void test13();
+    }
+}
+
+#endif // VECTORTEST_H_INCLUDED
