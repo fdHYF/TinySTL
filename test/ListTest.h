76029ef6f1937f09bace06f48ad97e27436f501d
hyf <37368470+fdHYF@users.noreply.github.com>
2019-03-13 17:14:08 +0800

    Create ListTest.h

---
+++ test/ListTest.h
@@ -0,0 +1,24 @@
+#ifndef LISTTEST_H_INCLUDED
+#define LISTTEST_H_INCLUDED
+
+#include "../list.h"
+#include "TestHelp.h"
+#include <string>
+#include <iostream>
+
+namespace TinySTL
+{
+    namespace ListTest
+    {
+        void runAll();
+
+        void test1();
+        void test2();
+        void test3();
+        void test4();
+        void test5();
+        void test6();
+    }
+}
+
+#endif // LISTTEST_H_INCLUDED
